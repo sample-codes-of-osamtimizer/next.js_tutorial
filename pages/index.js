d52b1196af228cef7e5833e8d2608ead9b58c51b
tokky <otokita@pepabo.com>
2020-04-12 19:27:33 +0900

    Clean URLS with dynamic Routing finished

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
-export default function Index() {
+import Layout from "../components/MyLayout";
+import Link from "next/link";
+
+const PostLink = (props) => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${props.id}`}>
+      <a>{props.id}</a>
+    </Link>
+  </li>
+);
+
+export default function Blog() {
   return (
-    <div>
-      <p>hello next.js</p>
-    </div>
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink id="Hello Next.js" />
+        <PostLink id="Learn Next.js is awesome" />
+        <PostLink id="Deploy apps with Zeit" />
+      </ul>
+    </Layout>
   );
 }
